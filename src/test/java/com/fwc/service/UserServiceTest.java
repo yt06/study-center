0a150cbab6b7c0b337eb3608f8eb2a5e1edbd618
fengweicao <fengweicao@2dfire.com>
2018-01-24 17:19:27 +0800

    author

--- src/test/java/com/fwc/service/UserServiceTest.java
+++ src/test/java/com/fwc/service/UserServiceTest.java
@@ -2,9 +2,6 @@ package com.fwc.service;
 
 import java.util.concurrent.ExecutionException;
 
-/**
- * Created by fengweicao on 2017/8/24.
- */
 public class UserServiceTest {
 
     public static void main (String[] args) throws ExecutionException, InterruptedException {
---
+++ src/test/java/com/fwc/service/test.java
@@ -0,0 +1,8 @@
+package com.fwc.service;
+
+/**
+ * @author yt
+ * @date 2018/1/24
+ */
+public class test {
+}
